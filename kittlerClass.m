b1092390755bdcfdd5758c16ef663a35fea3d006
gabosv <gaboasov@hotmail.com>
2019-09-18 22:10:33 -0600

    Implementation of getMu.

--- kittlerClass.m
+++ kittlerClass.m
@@ -1,69 +1,47 @@
 function kittler
     U = rgb2gray(imread('cuadro1_005.bmp'));    
     histU = getHistogram(U);
-    hn = HistogramNormalized(U)
     figure(1);
     plot(histU);
-    figure(2);
-    plot(hn);
     P = getP(histU, 1, 255)
+    mu = getMu(histU, 1, 255, P)
 %     kittlerThresh(histU);
 end
-
+ 
 function histU = getHistogram(U)
-    histU = double(histc(U(:), 0:255)) / size(U(:), 1);
+   histU = double(histc(U(:), 0:255)) / size(U(:), 1);
 end
-function hn = HistogramNormalized(U)
-    [f,c]=size(U);
-    hist = Histogram(U);
-    histn = hist/(f*c);
-    hn = histn;
-end
-function hist = Histogram (U)
-    img = double(U);
-    [f,c]=size(img);
-    for i=1:256
-        h(i) = 0;
-    end
-    for i=1:f
-       for j=1:c
-           k = img(i,j);
-           h(k+1) = h(k+1)+1;
-       end
-    end
-    hist = h; 
-end
-
-
+ 
 %calculates the cum sum of a histogram from a to b
 function P = getP(histU, a, b)
    P = cumsum(histU(a:b), 1)
 end
-
+ 
 %calculates the mean in the given interval
 function mu = getMu(histU, a, b, P)    
-    
+     mu = sum(histU(a:b) .* (a:b)) * P;
 end
-
+ 
 %calculates the variance in a given interval
 function varU = getVar(histU, a, b, P, mu)
     
 end
-
+ 
 %gets the likelihood of a given threshold, using kittler
 function [Jth, P1, mu1, var1, P2, mu2, var2] = getLikelihood(histU, th)
    
     
 end
-
-
+ 
+ 
 %kittler
 function [thOpt, gmmParams] = kittlerThresh(histU)
     
-
+ 
 end
-
+ 
 %applies the given treshold to the image
 function Th = applyThreshold(U, tau)
    
 end
+
